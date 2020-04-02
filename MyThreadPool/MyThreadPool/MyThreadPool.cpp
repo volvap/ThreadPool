c29021afddc41cde5a7f0d2ceadf50b1faf82266
Vladislav Pavlov <48180431+volvap@users.noreply.github.com>
2020-04-02 14:38:34 +0300

    Add files via upload

--- MyThreadPool/MyThreadPool/MyThreadPool.cpp
+++ MyThreadPool/MyThreadPool/MyThreadPool.cpp
@@ -1,8 +1,56 @@
+#include <condition_variable>
 #include <functional>
 #include <vector>
 #include <thread>
 
+class ThreadPoolClass {
+public :
+	explicit ThreadPoolClass(std::size_t NumThreads) {
+		start(NumThreads);
+	}
+	~ThreadPoolClass() {
+		stop();
+	}
+
+private:
+	std::vector<std::thread> MThreads;
+
+	std::condition_variable MEventVar;
+
+	std::mutex MEventMutex;
+	bool stopping = false;
+	void start(std::size_t NumThreads) {
+		for (size_t i = 0u; i < NumThreads; ++i)
+		{
+			MThreads.emplace_back([=] {
+				while (true) {
+					std::unique_lock<std::mutex> lock{ MEventMutex };
+					MEventVar.wait(lock, [=] {return stopping; });
+
+					if (stopping)
+						break;
+				}
+
+			});
+		}
+	}
+
+	void stop() noexcept {
+		{
+			std::unique_lock<std::mutex> lock{ MEventMutex };
+			stopping = true;
+		}
+		MEventVar.notify_all();
+
+		for (auto &thread : MThreads)
+			thread.join();
+
+			
+	}
+};
+
 int main()
 {
+	ThreadPoolClass(36);
 	return 0;
 }
